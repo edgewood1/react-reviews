dd26d05f36add7f4619d6fd81c856247021556cd
Mel DeJesus <meldejesus22@gmail.com>
2022-05-13 12:30:19 -0400

    linted api file

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,38 +1,33 @@
-export const read = (id=null) => {
-  return Api('GET', id);
-}
-
-export const update = (id, payload) => {
-  return Api('PUT', id, payload);
-}
-
 /**
  * Api
  * @param method {string} GET or PUT
  * @param id {string | null} id of response from the Details page
- * @param payload {string | object | null} 
+ * @param payload {string | object | null}
  * @returns {Promise}
  * @description this currently handles 3 options
  */
-const Api = async (method, id, payload=null) => {
-  let url =  `http://localhost:8000/reviews`
+const Api = async (method, id, payload = null) => {
+  let url = 'http://localhost:8000/reviews';
   url = (id) ? `${url}/${id}` : url;
 
   const options = {
-    method: method,
+    method,
     headers: { 'Content-Type': 'application/json' },
-  } 
+  };
 
   if (payload) {
     options.body = JSON.stringify(payload);
   }
-  
-  try{
+
+  try {
     const response = await fetch(url, options);
     return await response.json();
-  } catch(error) {
+  } catch (error) {
     console.log('error------', error);
-    return error
+    return error;
   }
-}
+};
 
+export const read = (id = null) => Api('GET', id);
+
+export const update = (id, payload) => Api('PUT', id, payload);
